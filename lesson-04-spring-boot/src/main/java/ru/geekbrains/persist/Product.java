c4b574ef1b87c40e2ff0a897686733472c0de4af
Alex <paalse@list.ru>
2021-02-08 16:37:58 +0400

    lesson-04 home work

---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,57 @@
+package ru.geekbrains.persist;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Product {
+    private Long id;
+
+    @NotEmpty
+    private String name;
+
+    private String description;
+
+    @NotNull
+    private int price;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/ProductRepository.java
@@ -0,0 +1,46 @@
+package ru.geekbrains.persist;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ProductRepository {
+    private Map<Long, Product> productMap = new ConcurrentHashMap<>();
+
+    private AtomicLong identity = new AtomicLong(0);
+
+    @PostConstruct
+    public void init() {
+        this.insert(new Product("Product1", "Description product1", 95));
+        this.insert(new Product("Product2", "Description product2", 105));
+        this.insert(new Product("Product3", "Description product3", 125));
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    public Product findById(Long id) {
+        return productMap.get(id);
+    }
+
+    public void insert(Product product) {
+        long id= identity.incrementAndGet();
+        product.setId(id);
+        productMap.put(id, product);
+    }
+
+    public void update(Product product) {
+        productMap.put(product.getId(), product);
+    }
+
+    public void delete(long id) {
+        productMap.remove(id);
+    }
+}
