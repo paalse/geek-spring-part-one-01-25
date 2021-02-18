cf825127c19547d59d4de0264a09f3589b3eeaf1
Alex <paalse@list.ru>
2021-02-18 10:20:34 +0400

    lesson-05 home work refactoring

--- lesson-05-hibernate/src/main/java/ru/geekbrains/Main05.java
+++ lesson-05-hibernate/src/main/java/ru/geekbrains/Main05.java
@@ -11,7 +11,7 @@ import javax.persistence.EntityManagerFactory;
 import java.util.List;
 
 
-public class Main {
+public class Main05 {
     public static void main(String[] args) {
         EntityManagerFactory emFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
